a6b6d6fd4c5b4a9ad3f5f4ab94f71f95ad34d49e
macsag <maciejsagata@gmail.com>
2019-11-19 12:13:26 +0100

    added viaf id processing, changed to marcxml

--- indexer/authority_indexer.py
+++ indexer/authority_indexer.py
@@ -1,10 +1,10 @@
 from tqdm import tqdm
-from pymarc import MARCReader
+from utils.permissive_marc_reader import PermissiveMARCReader
 import logging
 
 from config.indexer_config import AUTHORITY_INDEX_FIELDS
-from utils.indexer_utils import get_nlp_id, get_mms_id
-from utils.marc_utils import get_rid_of_punctuation
+from utils.indexer_utils import get_nlp_id, get_mms_id, get_viaf_id
+from utils.marc_utils import prepare_name_for_indexing
 
 
 def create_authority_index(data):
@@ -17,15 +17,16 @@ def create_authority_index(data):
     authority_index = {}
 
     with open(data, 'rb') as fp:
-        rdr = MARCReader(fp, to_unicode=True, force_utf8=True, utf8_handling='ignore')
+        rdr = PermissiveMARCReader(fp, to_unicode=True, force_utf8=True, utf8_handling='ignore')
         for rcd in tqdm(rdr):
             for fld in AUTHORITY_INDEX_FIELDS:
                 if fld in rcd:
-                    heading = get_rid_of_punctuation(rcd.get_fields(fld)[0].value())
+                    heading = prepare_name_for_indexing(rcd.get_fields(fld)[0].value())
                     nlp_id = get_nlp_id(rcd)
                     mms_id = get_mms_id(rcd)
+                    viaf_id = get_viaf_id(rcd)
 
-                    authority_index.setdefault(heading, {}).update({nlp_id: mms_id})
+                    authority_index.setdefault(heading, {}).update({nlp_id: {'mms_id': mms_id, 'viaf_id': viaf_id}})
                     authority_index[nlp_id] = heading
                     logging.debug(f'Zaindeksowano: {heading} | {authority_index[heading]}')
                     logging.debug(f'Zaindeksowano: {nlp_id} | {heading}')
