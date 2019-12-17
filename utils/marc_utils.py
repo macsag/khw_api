a3cc8a20045b88a980013dc0696e2e56b411c018
macsag <maciejsagata@gmail.com>
2019-12-17 07:52:02 +0100

    remove unused funcs added external ids added viaf and coords

--- utils/marc_utils.py
+++ utils/marc_utils.py
@@ -1,7 +1,5 @@
 import re
-
 import requests
-from pymarc import MARCReader
 
 from config.indexer_config import FIELDS_TO_CHECK
 
@@ -18,43 +16,6 @@ def prepare_name_for_indexing(value):
     return value
 
 
-def get_rid_of_punctuation(string):
-    return ''.join(char.replace(',', '').replace('.', '') for char in string)
-
-
-def read_marc_from_binary(data_chunk):
-    marc_rdr = MARCReader(data_chunk, to_unicode=True, force_utf8=True, utf8_handling='ignore')
-    for rcd in marc_rdr:
-        return rcd
-
-
-def calculate_check_digit(record_id):
-    char_sum = 0
-    i = 2
-    for character in record_id[::-1]:
-        char_sum += int(character) * i
-        i += 1
-    remainder = char_sum % 11
-    check_digit = str(remainder) if remainder != 10 else 'x'
-    return record_id + check_digit
-
-
-def get_marc_authority_data_from_data_bn(records_ids):
-    records_ids_length = len(records_ids)
-
-    if records_ids_length <= 100:
-        ids_for_query = '%2C'.join(record_id for record_id in records_ids)
-        query = 'http://data.bn.org.pl/api/authorities.marc?id={}&limit=100'.format(ids_for_query)
-
-        result = bytearray(requests.get(query).content)
-        return result
-
-
-def get_single_marc_authority_record_from_data_bn(record_id):
-    query = 'http://data.bn.org.pl/api/authorities.marc?id={}'.format(record_id)
-    return bytearray(requests.get(query).content)
-
-
 def get_marc_bibliographic_data_from_data_bn(records_ids):
     records_ids_length = len(records_ids)
 
@@ -66,7 +27,7 @@ def get_marc_bibliographic_data_from_data_bn(records_ids):
         return result
 
 
-def process_record(marc_record, auth_index, identifier_type):
+def process_record(marc_record, auth_index, identifier_type, auth_external_ids_index):
     """
     Main processing loop for adding authority identifiers to bibliographic record.
     """
@@ -89,9 +50,16 @@ def process_record(marc_record, auth_index, identifier_type):
                     if identifier_type == 'mms_id':
                         single_identifier = list(auth_index.get(term_to_search).values())[0]["mms_id"]
                     if identifier_type == 'all_ids':
-                        all_ids.update({'nlp_id': list(auth_index.get(term_to_search).keys())[0],
+                        nlp_id = list(auth_index.get(term_to_search).keys())[0]
+
+                        all_ids.update({'nlp_id': nlp_id,
                                         'mms_id': list(auth_index.get(term_to_search).values())[0]["mms_id"],
-                                        'viaf_id': list(auth_index.get(term_to_search).values())[0]["viaf_id"]})
+                                        'viaf_uri': list(auth_index.get(term_to_search).values())[0]["viaf_id"],
+                                        'coords': list(auth_index.get(term_to_search).values())[0]["coords"]})
+
+                        ext_ids = auth_external_ids_index.get_ids(nlp_id)
+                        if ext_ids:
+                            all_ids.update(ext_ids)
 
                     if single_identifier:
                         marc_record.remove_field(raw_fld)
