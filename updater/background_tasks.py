d1deda287fadf9aa53c338a7716654407cb00989
macsag <maciejsagata@gmail.com>
2020-03-26 12:00:41 +0100

    removed updater_status_instance and unused do_bib_update

--- updater/background_tasks.py
+++ updater/background_tasks.py
@@ -1,6 +1,2 @@
-async def do_authority_update(updater_instance, conn_auth_int, updater_status_instance):
-    await updater_instance.update_authority_index(conn_auth_int, updater_status_instance)
-
-
-def do_bib_update(updater_instance, bib_index, updater_status_instance):
-    updater_instance.update_bibliographic_index(bib_index, updater_status_instance)
+async def do_authority_update(updater_instance, aiohttp_session, conn_auth_int):
+    await updater_instance.update_authority_index(aiohttp_session, conn_auth_int)
