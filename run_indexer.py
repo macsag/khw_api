f501bd9cc648f62a16cb02bb0c83b82aa26ed337
macsag <maciejsagata@gmail.com>
2020-03-26 12:05:37 +0100

    uncomment creating authority index

--- run_indexer.py
+++ run_indexer.py
@@ -2,5 +2,5 @@ from indexer.authority_indexer import create_authority_index
 from indexer.authority_external_ids_indexer import AuthorityExternalIdsIndex
 
 
-#create_authority_index()
+create_authority_index()
 AuthorityExternalIdsIndex().index_in_redis()
