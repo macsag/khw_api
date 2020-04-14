5719b3cd2751982f10d973ab48943090f18b084e
macsag <maciejsagata@gmail.com>
2020-04-14 14:39:54 +0200

    add db flushing

--- run_indexer.py
+++ run_indexer.py
@@ -1,6 +1,6 @@
-from indexer.authority_indexer import create_authority_index
+from indexer.authority_indexer import create_authority_index, flush_db
 from indexer.authority_external_ids_indexer import AuthorityExternalIdsIndex
 
-
+flush_db()
 create_authority_index()
 AuthorityExternalIdsIndex().index_in_redis()
