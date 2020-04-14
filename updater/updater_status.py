3042879d469e76cde8c4a4024b4577b0c6226bb5
macsag <maciejsagata@gmail.com>
2020-04-14 14:39:34 +0200

    delete unused file

--- updater/updater_status.py
+++
@@ -1,6 +0,0 @@
-class UpdaterStatus(object):
-    def __init__(self, date_now):
-        self.update_in_progress = False
-
-        self.last_bib_update = date_now
-        self.last_auth_update = date_now
